98d78b7a0ad2dd3ad466ccad1735d2594620a834
kartik-soni1707 <its.kartiksoni@gmail.com>
2025-05-28 20:01:38 -0700

    1 layer

--- chat_bot.py
+++ chat_bot.py
@@ -1,7 +1,12 @@
 from secrets_1 import OPEN_AI_KEY
 import os 
-from langchain.llms import openai
-llm=openai()
 os.environ['OPENAI_API_KEY']=OPEN_AI_KEY
-name=llm("Fast car")
-print(name)
+
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+llm=OpenAI(temperature= 0.6)
+food_style_prompt=PromptTemplate(input_variables=['cuisine'],
+                                 template="I want to open restaurant for {cuisine} food. Suggest a unique name for this.")
+chain =LLMChain( llm=llm, prompt=food_style_prompt)
+chain.run("Indian")
