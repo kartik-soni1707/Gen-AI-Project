7baed8f24b7ec8f2e1243300950f75c62c28271f
kartik-soni1707 <its.kartiksoni@gmail.com>
2025-05-28 23:12:19 -0700

    simulator

--- chat_bot.py
+++ chat_bot.py
@@ -8,7 +8,7 @@ llm=OpenAI(temperature= 0.7)
 food_style_prompt=PromptTemplate(input_variables=['cuisine'],
                                  template="I want to open restaurant for {cuisine} food. Suggest one unique name for this.")
 menu_item_prompt=PromptTemplate(input_variables=['restaurant'],
-                                 template="Generate 5 dish names for the {restaurant} seperated by commas")
+                                 template="Generate 5 dish names for the {restaurant}. Return it as a comma separated list (dont list it)")
 name_chain = food_style_prompt|llm
 menu_chain = menu_item_prompt|llm
 restaurant= name_chain.invoke({"cuisine":"Indian"})
