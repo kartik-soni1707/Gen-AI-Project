8a53ebf312ba05dc6594590c158d44728e0ed7c1
kartik-soni1707 <its.kartiksoni@gmail.com>
2025-05-28 20:12:26 -0700

    menu items

--- chat_bot.py
+++ chat_bot.py
@@ -7,6 +7,10 @@ from langchain.prompts import PromptTemplate
 llm=OpenAI(temperature= 0.7)
 food_style_prompt=PromptTemplate(input_variables=['cuisine'],
                                  template="I want to open restaurant for {cuisine} food. Suggest one unique name for this.")
-chain = food_style_prompt|llm
-response= chain.invoke({"cuisine":"INdian"})
-print(response)
+menu_item_prompt=PromptTemplate(input_variables=['restaurant'],
+                                 template="Generate 5 dish names for the {restaurant} seperated by commas")
+name_chain = food_style_prompt|llm
+menu_chain = menu_item_prompt|llm
+restaurant= name_chain.invoke({"cuisine":"Indian"})
+food_items= menu_chain.invoke({"restaurant":restaurant})
+print(restaurant+food_items)
