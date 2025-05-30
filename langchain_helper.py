4b9c4c1e4554b9bf882e27d808cc512a4d6074a9
kartik-soni1707 <its.kartiksoni@gmail.com>
2025-05-29 20:11:33 -0700

    sending BE response

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,4 +1,20 @@
+from secrets_1 import OPEN_AI_KEY
+import os 
+os.environ['OPENAI_API_KEY']=OPEN_AI_KEY
+
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
 
 
 def generate_restaurant_name_and_items(cuisine):
-    return {"restaurant_name": "Lalal", "menu_items":"Pasta, pizza, meow"}
+    llm=OpenAI(temperature= 0.7)
+    food_style_prompt=PromptTemplate(input_variables=['cuisine'],
+                                    template="I want to open restaurant for {cuisine} food. Suggest one unique name for this.")
+    menu_item_prompt=PromptTemplate(input_variables=['restaurant'],
+                                    template="Generate 5 dish names for the {restaurant}. Return it as a comma separated list (dont list it)")
+    name_chain = food_style_prompt|llm
+    menu_chain = menu_item_prompt|llm
+    restaurant= name_chain.invoke({"cuisine":cuisine})
+    food_items= menu_chain.invoke({"restaurant":restaurant})
+   
+    return {"restaurant_name": restaurant, "menu_items":food_items}
